b876d0ca8bac65ad31d5df5014a8e6e278f23767
Benjamin Borbe <bborbe@rocketnews.de>
2015-04-01 15:55:28 +0200

    fix url parsing

--- bin/grep_urls/grep_urls_test.go
+++ bin/grep_urls/grep_urls_test.go
@@ -10,13 +10,28 @@ import (
 func TestDo(t *testing.T) {
 	var err error
 	writer := io_mock.NewWriter()
-	input := io_mock.NewReadCloserString("  http://www.exmaple.com  ")
+	input := io_mock.NewReadCloserString(" http://www.example.com ")
 	err = do(writer, input)
 	err = AssertThat(err, NilValue())
 	if err != nil {
 		t.Fatal(err)
 	}
-	err = AssertThat(string(writer.Content()), Is("http://www.exmaple.com\n"))
+	err = AssertThat(string(writer.Content()), Is("http://www.example.com\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDoBugfix(t *testing.T) {
+	var err error
+	writer := io_mock.NewWriter()
+	input := io_mock.NewReadCloserString("\n2015-04-01T15:40:09 http://www.example.com\n2015-04-01T15:40:09 http://www.example.com\n")
+	err = do(writer, input)
+	err = AssertThat(err, NilValue())
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = AssertThat(string(writer.Content()), Is("http://www.example.com\nhttp://www.example.com\n"))
 	if err != nil {
 		t.Fatal(err)
 	}
