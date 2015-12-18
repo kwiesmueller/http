b9ac42d187289ab3a3e5d51e3e82f0b5a4a4b118
Benjamin Borbe <bborbe@rocketnews.de>
2015-12-19 00:08:25 +0100

    update jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -2,29 +2,31 @@
 
 SOURCEDIRECTORY="github.com/bborbe/http"
 
+################################################################################
+
 export GOROOT=/opt/go1.5.1
-export PATH=$GOROOT/bin:$PATH
+export PATH=/opt/utils/bin/:/opt/go2xunit/bin/:$GOROOT/bin:$PATH
 export GOPATH=${WORKSPACE}
 export REPORT_DIR=${WORKSPACE}/test-reports
 DEB="${NAME}_${VERSION}.deb"
 rm -rf $REPORT_DIR ${WORKSPACE}/*.deb ${WORKSPACE}/pkg
 mkdir -p $REPORT_DIR
-PACKAGES=`cd src && find $SOURCEDIRECTORY -name "*_test.go" | /opt/utils/bin/dirof | /opt/utils/bin/unique`
+PACKAGES=`cd src && find $SOURCEDIRECTORY -name "*_test.go" | dirof | unique`
 FAILED=false
 for PACKAGE in $PACKAGES
 do
-    XML=$REPORT_DIR/`/opt/utils/bin/pkg2xmlname $PACKAGE`
-    OUT=$XML.out
-    go test -i $PACKAGE
-    go test -v $PACKAGE | tee $OUT
-    cat $OUT
-	/opt/go2xunit/bin/go2xunit -fail=true -input $OUT -output $XML
-	rc=$?
-	if [ $rc != 0 ]
-	then
-	    echo "Tests failed for package $PACKAGE"
-    	FAILED=true
-	fi
+  XML=$REPORT_DIR/`pkg2xmlname $PACKAGE`
+  OUT=$XML.out
+  go test -i $PACKAGE
+  go test -v $PACKAGE | tee $OUT
+  cat $OUT
+  go2xunit -fail=true -input $OUT -output $XML
+  rc=$?
+  if [ $rc != 0 ]
+  then
+    echo "Tests failed for package $PACKAGE"
+    FAILED=true
+  fi
 done
 
 if $FAILED
